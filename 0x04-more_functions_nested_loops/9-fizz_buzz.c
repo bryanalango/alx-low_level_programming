4b47af8fa1d7155234cc860145c8c2037a703967
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-24 21:02:06 +0300

    Fizz and Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point. Prints Fizz, Buzz, and FizzBuzz
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int multiples;
+
+	for (multiples = 1; multiples <= 100; multiples++)
+	{
+		if ((multiples % 3 == 0) && (multiples % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (multiples % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (multiples % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		while (multiples <= 100)
+		{
+			putchar(' ');
+			multiples++;
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
