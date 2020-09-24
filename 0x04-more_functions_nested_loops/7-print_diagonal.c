febbdce2d2da4f250ba70d19573d7968e0031f63
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-24 19:59:48 +0300

    Diagonal Line(s)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Entry Point. Prints Diagonal Line
+ * @n: Number of lines to be Printed
+ * Return: No return since function is void
+ */
+
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space < line; space++)
+			{
+				_putchar('\');
+				_putchar('\n');
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
