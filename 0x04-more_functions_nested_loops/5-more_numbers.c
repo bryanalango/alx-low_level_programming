2811bcdd975f5f6c8c1d6edd900142328f591ae7
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-24 18:29:19 +0300

    10 times numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+# include "holberton.h"
+
+/**
+ * void more_numbers(void) - Entry Point. Prints numbers from 0 -14 ten times
+ * @counter - Line Counter
+ * @number - First iteration
+ * @number_1 - Second Iteration
+ * Return: Returns nothing since function is void
+ */
+
+void more_numbers(void)
+{
+	int counter;
+	int numbers;
+	int numbers_1;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			numbers_1 = numbers;
+			if (numbers_1 >= 11 && numbers_1 <= 14)
+			{
+				_putchar(numbers_1 + '0');
+			}
+			_putchar((numbers_1 % 10) + '0')
+		}
+		_putchar('\n');
+	}
+
+
+}
