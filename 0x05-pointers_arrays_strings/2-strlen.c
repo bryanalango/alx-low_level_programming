d473f09230c339fd492e4244f1e5cf5dec085705
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-29 01:04:32 +0300

    Returns Length of a String

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Entry point. Returns the length of a string
+ * @s: string whose length should be returned
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int chars;
+
+	for (chars = 0; s[chars] != '\0'; chars++)
+	{
+	}
+	return (chars);
+}
