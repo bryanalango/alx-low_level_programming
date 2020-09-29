c2ced47a150f191085b050f58a9e6d8e5ccbfe1a
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-29 17:38:16 +0300

    Prints Half the String

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: pointer to a string to be printed
+ * Return: No return Value
+ */
+
+void puts_half(char *str)
+{
+	int a, b;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+	for (b = i / 2; str[b] != '\0'; b++)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n')
+}
+
