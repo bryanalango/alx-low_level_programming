fb235c0c649ca43a229ca3a0c2ab6e133baabb38
Bryan Alango <b.alango@launchpad.alx.app>
2020-10-01 04:45:58 +0300

    Copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Entry Point. Copying a string
+ * @dest: Destination String
+ * @src: Source input string
+ * @n: Possible elements of a string
+ * Return: Returns a copied destination string
+ *
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	len = 0;
+	while (src[len] != '\0' && len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+
+	while (len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+
+	return (dest);
+}
