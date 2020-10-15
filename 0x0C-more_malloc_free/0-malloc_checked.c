50e43af648010d4977596c958eeb8e0faae51f29
Bryan Alango <b.alango@launchpad.alx.app>
2020-10-15 20:44:26 +0300

    Allocates Memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,21 @@
+#include <stdlib.h>
 #include "holberton.h"
-#include "stdlib.h"
 
 /**
- * malloc_checked - calls exit on failure
- * @b: number of bytes to allocate
- *
- * Return: void
- */
-
+  * malloc_checked - function that allocates memory using malloc.
+  * @b: amount of memory of the array.
+  * Return: allocated pointer in the memory..
+  */
+  
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *p;
 
-	if (!p)
+	p = malloc(b);
+
+	if (p == NULL)
+	{
 		exit(98);
+	}
 	return (p);
 }
