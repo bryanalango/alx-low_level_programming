1d95ef6c3e9f8462289a40c5e6169f9c5e78e564
Bryan Alango <b.alango@launchpad.alx.app>
2020-10-19 21:11:42 +0300

    Frees Dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: dog to be freed
+ *
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
